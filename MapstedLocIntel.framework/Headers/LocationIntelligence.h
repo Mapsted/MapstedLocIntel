e6a6f65a0fa2a8ec64c43c0da4514ecc69f20017
Daniel Teklegiorgis <d.teklegiorgis@mapsted.com>
2020-07-23 11:06:02 -0400

    Committing first version of location intelligence framework

---
+++ MapstedLocIntel.framework/Headers/LocationIntelligence.h
@@ -0,0 +1,19 @@
+//
+//  LocationIntelligence.h
+//  LocationIntelligence
+//
+//  Created by Daniel on 2020-07-22.
+//  Copyright © 2020 Mapsted. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocationIntelligence.
+FOUNDATION_EXPORT double LocationIntelligenceVersionNumber;
+
+//! Project version string for LocationIntelligence.
+FOUNDATION_EXPORT const unsigned char LocationIntelligenceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationIntelligence/PublicHeader.h>
+
+
